09efe6d644349f46c8ef6d7573136786e3e24c05
Joel Lopez <djoel95@hotmail.com>
2025-04-29 14:29:47 -0400

    test15

--- app/api/user/add-address/route.js
+++ app/api/user/add-address/route.js
@@ -1,8 +1,8 @@
 
+import connectDB from "@/config/db"
 import Address from "@/models/Address"
 import { getAuth } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server"
-import connectDB from "@/config/db"
 
 
 
