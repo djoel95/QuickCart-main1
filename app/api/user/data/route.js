b812b5b1a203e3f6b0282e5345945b48464498a9
Joel Lopez <djoel95@hotmail.com>
2025-04-29 07:34:37 -0400

    test4

---
+++ app/api/user/data/route.js
@@ -0,0 +1,22 @@
+import connectDB from "@/config/db";
+import User from "@/models/User";
+import { getAuth } from "@clerk/nextjs/dist/types/server";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+    try {
+        const { userID } = getAuth(request)
+
+        await connectDB()
+        const user = await User.findById(userID)
+
+        if (!user) {
+            return NextResponse.json({ success: false, message: 'User not found' }, { status: 404 });
+        }
+
+        return NextResponse.json({ success: true, user});
+    } catch (error) {
+        return NextResponse.json({ success: false, message: error.message });
+    }
+}
+    
--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,8 +1,9 @@
 'use client'
 import { productsDummyData, userDummyData } from "@/assets/assets";
-import { useUser } from "@clerk/nextjs";
+import { useAuth, useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
@@ -16,6 +17,8 @@ export const AppContextProvider = (props) => {
     const router = useRouter()
 
     const { user } = useUser()
+    const { getToken } = useAuth()
+
 
     const [products, setProducts] = useState([])
     const [userData, setUserData] = useState(false)
@@ -27,10 +30,25 @@ export const AppContextProvider = (props) => {
     }
 
     const fetchUserData = async () => {
-        if (user.publicMetadata.role === 'seller') {
-            setIsSeller(true)
+        try {
+            if (user.publicMetadata.role === 'seller') {
+                setIsSeller(true)
+            }
+            const token = await getToken()
+            const {data} = await axios.get('/api/user/data', {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            if (data.success) {
+                setUserData(data.user)
+                setCartItems(data.user.cartItems)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error.message)
         }
-        setUserData(userDummyData)
 
     }
 
@@ -85,11 +103,13 @@ export const AppContextProvider = (props) => {
     }, [])
 
     useEffect(() => {
+        if (user) {
         fetchUserData()
-    }, [])
+        }
+    }, [user])
 
     const value = {
-        user, 
+        user, getToken,
         currency, router,
         isSeller, setIsSeller,
         userData, fetchUserData,
